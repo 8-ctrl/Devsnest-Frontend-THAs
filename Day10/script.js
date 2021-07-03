d2d7446bae81ac9cca1264278706b63d6ef79789
Neel <neelpatel1029@gmail.com>
2021-07-03 15:57:52 +0530

    Memory Game

---
+++ Day10/script.js
@@ -0,0 +1,56 @@
+const cards =document.querySelectorAll('.cards');
+
+let OneflippedCard = false;
+let lock = false;
+let firstCard, secondCard;
+var moves = 0
+let click = new Audio('mc.mp3');
+// let match_sound = new Audio('match.mp3');
+
+function flipCard(){
+    click.play();
+    if(lock) return;
+    if(this === firstCard) return;
+    
+    this.classList.add('flipped');
+    if(!OneflippedCard){
+        OneflippedCard=true;
+        firstCard=this;
+        return;
+    }
+    secondCard = this;
+    lock = true;
+    
+    match();
+}
+
+function match(){
+    moves+=1;
+    document.getElementById('jsmoves').innerText = moves
+    firstCard.dataset.name === secondCard.dataset.name ? disableCards()  : unflipCards();
+}
+
+function  disableCards(){
+    // match_sound.play();
+    firstCard.removeEventListener('click',flipCard);
+    secondCard.removeEventListener('click',flipCard);
+    reset();
+}
+
+function unflipCards(){
+    setTimeout(()=> {
+        firstCard.classList.remove('flipped');
+        secondCard.classList.remove('flipped');
+        reset();
+    },1500)
+
+}
+
+function reset(){
+    OneflippedCard= false;
+    lock=false;
+    firstCard=null;
+    secondCard= null;
+}
+cards.forEach(card => card.addEventListener('click',flipCard));
+
